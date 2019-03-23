38a313d84c43d8df9dafce58190f4a97aa47d539
tfchan <cseelemon@gmail.com>
2019-03-23 22:58:21 +0800

    Add fit function and complete prior calculation

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,5 @@
 """Module of naive Bayes."""
+import numpy as np
 
 
 class NBClassifier:
@@ -7,3 +8,14 @@ class NBClassifier:
     def __init__(self, mode=0):
         """Initialize classifier with mode."""
         self._mode = mode
+        self._prior = {}
+
+    def compute_prior(self, targets):
+        """Compute prior probabilty for each class."""
+        category, counts = np.unique(targets, return_counts=True)
+        counts = [(count, count / counts.sum()) for count in counts]
+        self._prior = dict(zip(category, counts))
+
+    def fit(self, features, targets):
+        """Fit the classifier with features and targets."""
+        self.compute_prior(targets)
